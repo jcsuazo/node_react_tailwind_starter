b9ff1f0496b8e1b1e0b526f4a52fa6140c082249
jcsuazo <jsuazo9@gmail.com>
2021-02-10 08:29:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# eCommerce-starting-point
+# node_react_starter
+# Ecommerce-starting-2
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { Row } from 'react-bootstrap';
+import Meta from '../components/Meta';
+
+const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword;
+  const pageNumber = match.params.pageNumber || 1;
+  const dispatch = useDispatch();
+
+  useEffect(() => {}, [dispatch, keyword, pageNumber]);
+
+  function showProducts() {
+    return (
+      <>
+        <Row>
+          <h1>Home</h1>
+        </Row>
+      </>
+    );
+  }
+  return (
+    <>
+      <Meta />
+      {!keyword ? (
+        showProducts()
+      ) : (
+        <Link to='/' className='btn btn-light'>
+          Go back
+        </Link>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
